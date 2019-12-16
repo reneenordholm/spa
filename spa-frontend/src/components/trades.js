8d2eb13ee907604bf4b12bed33eac5ee49476b9d
Renee Nordholm <reneenordholm@gmail.com>
2019-12-16 17:12:03 -0600

    layout progress

--- spa-frontend/src/components/trades.js
+++ spa-frontend/src/components/trades.js
@@ -24,14 +24,43 @@ class Trades {
                 const about = document.querySelector('div.about')
                 const pImg = document.createElement('IMG')
                 pImg.setAttribute("src", trades[trade]['img'])
+                pImg.setAttribute("style", "width:50%")
                 pImg.setAttribute("class", "box")
                 about.appendChild(pImg)
 
                 const p = document.createElement('p')
                 p.innerText = trades[trade]['description']
+                // p.setAttribute("style", "width:50%")
                 p. setAttribute("class", "box")
                 about.appendChild(p)
-            } 
+            } else if (trades[trade]['trade_type'] === "work") {
+                const work = document.querySelector('div.work')
+
+                const row = document.createElement('div')
+                row.setAttribute("class", "row")
+                work.appendChild(row)
+
+                const column = document.createElement('div')
+                column.setAttribute("class", "column")
+                row.appendChild(column)
+
+                const content = document.createElement('div')
+                content.setAttribute("class", "content")
+                column.appendChild(content)
+                
+                const wImg = document.createElement('IMG')
+                wImg.setAttribute("src", trades[trade]['img'])
+                wImg.setAttribute("style", "width:100%")
+                content.appendChild(wImg)
+
+                const wHeader = document.createElement('H3')
+                wHeader.innerText = trades[trade]['title']
+                content.appendChild(wHeader)
+
+                const wDesc = document.createElement('p')
+                wDesc.innerText = trades[trade]['description']
+                content.appendChild(wDesc)
+            }
         }
     }
 }
