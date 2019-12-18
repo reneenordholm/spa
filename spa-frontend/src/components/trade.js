76d506d118f73ed267ecd56e3e95eba0e90b48fc
Renee Nordholm <reneenordholm@gmail.com>
2019-12-18 16:49:06 -0600

    trade class refactoring

---
+++ spa-frontend/src/components/trade.js
@@ -0,0 +1,55 @@
+class Trade {
+    constructor(trade) {
+        this.trade_type = trade.trade_type
+        this.img = trade.img
+        this.description = trade.description
+        this.title = trade.title
+    }
+
+    renderTrade() {
+        // for each trade
+        // add object to page by type
+        if (this.trade_type === "about") {
+            const about = document.querySelector('div.about')
+            const dImg = document.createElement('div')
+            dImg.setAttribute("class", "dImg")
+            about.appendChild(dImg)
+
+            const pImg = document.createElement('IMG')
+            pImg.setAttribute("src", this.img)
+            pImg.setAttribute("style", "width:100%")
+            dImg.appendChild(pImg)
+
+            const dPar = document.createElement('div')
+            dPar.setAttribute("class", "dPar")
+            about.appendChild(dPar)
+
+            const pText = document.createElement('p')
+            pText.innerText = this.description
+            dPar.appendChild(pText)
+
+        } else if (this.trade_type === "work") {
+            const work = document.querySelector('div.work')
+            const column = document.createElement('div')
+            column.setAttribute("class", "column")
+            work.appendChild(column)
+
+            const content = document.createElement('div')
+            content.setAttribute("class", "content")
+            column.appendChild(content)
+            
+            const wImg = document.createElement('IMG')
+            wImg.setAttribute("src", this.img)
+            wImg.setAttribute("style", "width:100%")
+            content.appendChild(wImg)
+
+            const wHeader = document.createElement('H3')
+            wHeader.innerText = this.title
+            content.appendChild(wHeader)
+
+            const wDesc = document.createElement('p')
+            wDesc.innerText = this.description
+            content.appendChild(wDesc)
+        }
+    }
+}
--- spa-frontend/src/components/trades.js
+++ spa-frontend/src/components/trades.js
@@ -1,6 +1,6 @@
 class Trades {
     constructor() {
-        // this.trades = []
+        this.trades = []
         this.adapter = new TradesAdapter()
         // this.bindEventListeners()
         this.fetchAndLoadTrades()
@@ -11,57 +11,16 @@ class Trades {
     fetchAndLoadTrades() {
         this.adapter
             .getTrades()
-            .then(trades => 
-                this.renderTrades(trades))
+            .then(trades => {
+                trades.forEach(trade => this.trades.push(new Trade(trade)))
+            })
+            .then(() => {
+                this.renderTrades()
+            })
     }
 
     // render all trades appropriately
-    renderTrades(trades) {
-        // for each trade
-        for(const trade in trades){
-            // add object to page by type
-            if (trades[trade]['trade_type'] === "about") {
-                const about = document.querySelector('div.about')
-                const dImg = document.createElement('div')
-                dImg.setAttribute("class", "dImg")
-                about.appendChild(dImg)
-
-                const pImg = document.createElement('IMG')
-                pImg.setAttribute("src", trades[trade]['img'])
-                pImg.setAttribute("style", "width:100%")
-                dImg.appendChild(pImg)
-
-                const dPar = document.createElement('div')
-                dPar.setAttribute("class", "dPar")
-                about.appendChild(dPar)
-
-                const pText = document.createElement('p')
-                pText.innerText = trades[trade]['description']
-                dPar.appendChild(pText)
-                
-            } else if (trades[trade]['trade_type'] === "work") {
-                const work = document.querySelector('div.work')
-                const column = document.createElement('div')
-                column.setAttribute("class", "column")
-                work.appendChild(column)
-
-                const content = document.createElement('div')
-                content.setAttribute("class", "content")
-                column.appendChild(content)
-                
-                const wImg = document.createElement('IMG')
-                wImg.setAttribute("src", trades[trade]['img'])
-                wImg.setAttribute("style", "width:100%")
-                content.appendChild(wImg)
-
-                const wHeader = document.createElement('H3')
-                wHeader.innerText = trades[trade]['title']
-                content.appendChild(wHeader)
-
-                const wDesc = document.createElement('p')
-                wDesc.innerText = trades[trade]['description']
-                content.appendChild(wDesc)
-            }
-        }
+    renderTrades() {
+        return this.trades.map(trade => trade.renderTrade())
     }
 }
