d9157c529107579d7b23eb3a578a76b2f4c71d29
Renee Nordholm <reneenordholm@gmail.com>
2019-12-22 15:06:46 -0600

    login listener setup

--- spa-api/app/controllers/application_controller.rb
+++ spa-api/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
-    # include ActionController::Cookies
-    # include ActionController::RequestForgeryProtection
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
   
-    # protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
 
     # private
 
--- spa-frontend/src/login.js
+++ spa-frontend/src/login.js
@@ -9,8 +9,37 @@ window.onclick = function(event) {
     }
 }
 
-// POST request to login
+const submit = document.getElementById('login-button')
+submit.addEventListener('click', event => {
+    postTest(event)
+  })
 
+// POST request to login
+// 'POST' request to create new toy objects
+function postTest(event) {
+    event.preventDefault(event)
+    const form = document.getElementsByClassName('container')
+
+    console.log(event)
+    console.log(form)
+    return fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        email: event.email.value,
+        password: event.password.value
+      })
+    })
+    .then(function(response) { 
+      return response.json()
+    })
+    .then(function(json) {
+      console.loog(json)
+    })
+  }
 
 // DELETE request to logout 
 
